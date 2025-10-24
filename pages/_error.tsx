f77c4c6b0e7081cb971a6c8c33fc3b2e28f478eb
Rob Morris <rob@prismatik.com.au>
2025-10-25 10:29:33 +1100

    Remove semantic-ui from _error.tsx too

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,41 +1,76 @@
+// pages/_error.tsx
 import React from 'react'
 import Link from 'next/link'
-import { Grid } from 'semantic-ui-react'
+import { NextPageContext } from 'next'
 
-//
-// Generic error page
-// https://nextjs.org/docs/advanced-features/custom-error-page
-//
+function ErrorPage({ statusCode }: { statusCode?: number }) {
+  let errorMessage: React.ReactNode
 
-ErrorPage.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-  return { statusCode }
+  if (statusCode === 404) {
+    errorMessage = '404: Page not found'
+  } else if (statusCode) {
+    errorMessage = (
+      <div>
+        A <b>{statusCode}</b> server error occurred
+      </div>
+    )
+  } else {
+    errorMessage = <div>A client error occurred</div>
+  }
+
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        inset: 0, // full viewport
+        backgroundColor: '#1d1c20', // match dark app bg
+        color: '#f5f5f5',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        padding: '2rem',
+      }}
+    >
+      <h3
+        style={{
+          margin: '0 0 1rem 0',
+          fontSize: '1.5rem',
+          fontWeight: 500,
+        }}
+      >
+        {errorMessage}
+      </h3>
+
+      <Link
+        href="/"
+        style={{
+          display: 'inline-block',
+          textDecoration: 'none',
+          color: 'white',
+          backgroundColor: '#0070f3',
+          padding: '0.6rem 1.2rem',
+          borderRadius: '4px',
+          fontWeight: 500,
+          transition: 'background-color 0.2s ease',
+        }}
+      >
+        Home
+      </Link>
+    </div>
+  )
 }
 
-function ErrorPage({ statusCode }) {
-  const errorMessage =
-    parseInt(statusCode) == 404 ? `404: Page not found`
-      : statusCode ? <div>A <b>{statusCode}</b> server error occurred</div>
-        : <div>A client error occurred</div>;
-  return (
-    <div className='App'>
-
-      <div className='AlignTop'>
-        <hr />
-
-        <h3>{errorMessage}</h3>
-        {/* <p>If the error persists, please <Link href='/help#connect'>contact us</Link></p> */}
-
-        <hr />
-        <Grid columns={2}>
-          <Grid.Column className='AlignCenter'>
-            <Link href='/'>Home</Link>
-          </Grid.Column>
-        </Grid>
-      </div>
-
-    </div>
-  );
+// getInitialProps is still supported for _error in the pages router.
+// This preserves your statusCode logic.
+ErrorPage.getInitialProps = ({ res, err }: NextPageContext) => {
+  const statusCode = res
+    ? res.statusCode
+    : err
+      ? (err as any).statusCode
+      : 404
+  return { statusCode }
 }
 
 export default ErrorPage
